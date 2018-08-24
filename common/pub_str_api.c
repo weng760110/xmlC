c175344a9156a3688a6a4474bb0a11a891ce689e
weng760110 <30426705+weng760110@users.noreply.github.com>
2018-08-24 13:56:24 +0900

    Update pub_str_api.c

--- common/pub_str_api.c
+++ common/pub_str_api.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+char*
+replace_string(char* str, char* a, char* b)
+{
+    int len  = strlen(str);
+    int lena = strlen(a);
+    int lenb = strlen(b);
+    char* p = NULL;
+
+    for (p = str; p = strstr(p, a), p != NULL; ++p) {
+        if (lena != lenb) {
+            memmove(p+lenb, p+lena, len - (p - str) + lenb);
+        }
+        memcpy(p, b, lenb);
+    }
+    return str;
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+char*
+replace_string(char* str, char* a, char* b)
+{
+    int len  = strlen(str);
+    int lena = strlen(a);
+    int lenb = strlen(b);
+    char* p = NULL;
+
+    for (p = str; p = strstr(p, a), p != NULL; ++p) {
+        if (lena != lenb) {
+            memmove(p+lenb, p+lena, len - (p - str) + lenb);
+        }
+        memcpy(p, b, lenb);
+    }
+    return str;
+}
 
